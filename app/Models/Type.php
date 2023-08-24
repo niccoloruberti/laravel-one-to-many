9af1048e56900cf019790eefa0091939cfe84dd0
niccoloruberti <nikiruberti@gmail.com>
2023-08-24 15:27:57 +0200

    creato model Type con migration e seeder

---
+++ app/Models/Type.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Type extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'slug'];
+
+    public static function generateSlug($name) {
+        
+        return Str::slug($name, '-');
+    }
+}
---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Type;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i = 1; $i <= 10; $i++) {
+
+            $type = new Type();
+
+            $type->name = $faker->randomElement(['Frontend', 'Backend']);
+            $type->slug = $type->generateSlug($type->name);
+
+            $type->save();
+        }
+    }
+}
